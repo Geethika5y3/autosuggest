38c34080de443ec2323421ac99bd3a8981a71c31
Geethika5y3 <24wh1a05y3@bvrithyderabad.edu.in>
2026-07-03 13:49:23 +0530

    Updated server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT||3002;
+app.use(express.static('frontend'));
+app.listen(port,function(){
+    console.log("successfully running at http://localhost:"+port);
+});
